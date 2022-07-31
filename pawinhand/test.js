fcbe241d2c2bc328a5f553a0a8aec0d8bd622230
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-07-31 11:43:05 +0900

    스크롤 이동에 따른 페이지 변화 및 스크롤 부드러운 이벤트

--- pawinhand/test.js
+++ pawinhand/test.js
@@ -1,11 +1,46 @@
-$(document).ready(function(){
+const MAX_PAGE = 3;
+const MIN_PAGE = 0;
+// const SCROLL_GAGE = 2;
+
+const divs = document.querySelectorAll('div');
+console.log(divs);
+let page = MIN_PAGE;
+
+window.addEventListener('wheel', (e) => {
+  e.preventDefault();
+  if(e.wheelDelta === 120) {
+    console.log("휠 델타 : " ,e.wheelDelta);
+    console.log("스크롤 업");
+    console.log(page);  
+    if(page === MIN_PAGE) return;
+    else {
+      page--;
+      console.log("바뀐 페이지 : ", page);  
+      scroll({
+        top: window.innerHeight * page,
+        left: 0,
+        behavior: "smooth",
+      })
+    }
+  } 
+  else {
+    console.log("휠 델타 : " ,e.wheelDelta);
+    console.log("스크롤 다운");
+    console.log(page);  
+    if(page === MAX_PAGE) return;
+    else {
+      page++;
+      console.log("바뀐 페이지 : ", page);  
+      scroll({
+        top: window.innerHeight * page,
+        left: 0,
+        behavior: "smooth",
+      })
+    }
+  }
   
-  $("h1").on('click', function() {
-//    $(":root").css({"--bg-color": "yellow", "--color": "black"});
-   console.log($(":root").css("--bg-color"));
-   console.log($("h1").css("color"));
-    // document.documentElement.style.setProperty("--bg-color", "yellow");
-    // document.documentElement.style.setProperty("--y", e.clientY + "px");
-  });
-  
-});
+}, {passive : false}); // preventDefault를 먹히게 하는 옵션
+
+window.addEventListener('beforeunload', (e) => {
+  scrollTo(0, 0);
+})
